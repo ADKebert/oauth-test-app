91bd90477f3afd9321c9f538c1926a47378188d8
Alan Kebert <adkebert@yahoo.com>
2016-09-19 17:15:16 -0400

    copied over omniauth stuff

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,14 @@ Rails.application.routes.draw do
   get 'pages/home'
   root 'pages#home'
 
+  # OAuth stuff
+  get '/auth/:provider'          => 'omniauth#auth', as: :auth
+  get '/auth/:provider/callback' => 'session#create'
+
+  # Aliasing session stuff
+  get '/login'  => 'session#new'
+  post '/login' => 'session#create'
+  get '/logout' => 'session#destroy'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
